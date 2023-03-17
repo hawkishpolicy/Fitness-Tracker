474765a9cba574e4de5d7043d862d584f14c34b6
PolyNoodle94 <emidollar@hotmail.com>
2023-03-17 12:32:49 -0500

    added some routes to activities

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const activitiesRouter = express.Router();
 
 // GET /api/activities
+activitiesRouter.get("/", async (req, res, next) => {});
 
 // GET /api/activities/:activityId/routines
+activitiesRouter.get("/:activityId/routines", async (req, res, next) => {});
 
 // POST /api/activities
+activitiesRouter.post("/", async (req, res, next) => {});
 
 // PATCH /api/activities/:activityId
+activitiesRouter.patch("/:activityId", async (req, res, next) => {});
 
 module.exports = activitiesRouter;
